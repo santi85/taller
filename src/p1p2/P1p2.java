cfed9557bd501a4910937e03301ed28469801783
LDD02 <LDD02@LDD02.estudiantes.uco>
2016-07-26 20:57:33 -0500

    version 3.0 del ejercicio 1

--- src/p1p2/P1p2.java
+++ src/p1p2/P1p2.java
@@ -16,7 +16,7 @@ public class P1p2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola github V2.0");
+        System.out.println("Hola github V3.0");
     }
     
 }
